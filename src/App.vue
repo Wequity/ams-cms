4f2e0e42e74285d2a6221fd0c7f38aca6385e142
emilien-wequity <emilien@wequity.app>
2022-05-04 11:08:29 +0200

    chore: move cms from app to its own repo

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>Nothing to see here.</span>
+</template>
+
+<style>
+body {
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  margin: 0;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
